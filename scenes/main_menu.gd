c54c8b834e1b1c15a6f756c01e31604f1cc9d2e0
Ken San Angelo <ken@kensanangelo.com>
2023-06-12 17:12:21 -0400

    Updates buttons to match UI, including fonts and background, and adds tileset to menus to make it look a little nicer

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -5,10 +5,8 @@ extends Control
 @export var how_to_play_button: Button
 @export var options_button: Button
 
-
 func _ready() -> void:
 	two_player_button.clicked.connect(on_two_player_button_pressed)
-
 
 func on_two_player_button_pressed() -> void:
 	SceneManager.start_battle(2)
